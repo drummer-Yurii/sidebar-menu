95aa10744dcec5af373b7de9b4403348020b05b8
Yurii Rybachok <drummer2009@ukr.net>
2023-11-08 13:03:52 +0200

    styling sidebar & add logo

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,6 +1,8 @@
 <template>
     <aside>
-        Sidebar
+        <div class="logo">
+            <img src="../assets/vue.svg" alt="Vue">
+        </div>
     </aside>
 </template>
 
@@ -9,5 +11,29 @@
 </script>
 
 <style lang="scss" scoped>
+aside {
+    display: flex;
+    flex-direction: column;
+    width: calc(2rem + 32px);
+    overflow: hidden;
+    min-height: 100vh;
+    padding: 1rem;
 
+    background-color: var(--dark);
+    color: var(--light);
+    transition: 0.2s ease-out;
+
+    .logo {
+        margin-bottom: 1rem;
+
+        img {
+            width: 2rem;
+        }
+    }
+
+    @media (max-width: 768px) {
+        position: fixed;
+        z-index: 99;
+    }
+}
 </style>
