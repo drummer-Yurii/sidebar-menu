3b7e683b7dcb6976c16d358fafb4bdb63786624c
Yurii Rybachok <drummer2009@ukr.net>
2023-11-08 13:23:53 +0200

    add arrow for open & close sidebar

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,13 +1,24 @@
 <template>
-    <aside>
+    <aside :class="`${is_expanded && 'is-expanded'}`">
         <div class="logo">
             <img src="../assets/vue.svg" alt="Vue">
+        </div>
+        <div class="menu-toggle-wrap">
+            <button class="menu-toggle" @click="ToggleMenu">
+                <span class="material-icons">keyboard_double_arrow_right</span>
+            </button>
         </div>
     </aside>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const is_expanded = ref(false);
+
+const ToggleMenu = () => {
+    is_expanded.value = !is_expanded.value;
+}
 </script>
 
 <style lang="scss" scoped>
@@ -28,6 +39,44 @@ aside {
 
         img {
             width: 2rem;
+        }
+    }
+
+    .menu-toggle-wrap {
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 1rem;
+        position: relative;
+        top: 0;
+        transition: 0.2s ease-out;
+
+        .menu-toggle {
+            transition: 0.2s ease-out;
+
+            .material-icons {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            &:hover {
+                .material-icons {
+                    color: var(--primary);
+                    transform: translateX(0.5rem);
+                }
+            }
+        }
+    }
+
+    &.is-expanded {
+        width: var(--sidebar-width);
+
+        .menu-toggle-wrap {
+            top: -3rem;
+
+            .menu-toggle {
+                transform: rotate(-180deg);
+            }
         }
     }
 
